6a13ef80cad4f164e00aa0078bd7adfaf250ba28
Anik Rahman Chowdhury <111109249+anikrahman007@users.noreply.github.com>
2024-09-17 12:20:50 +0600

    variable_01.py

---
+++ PYTHON/variable_01.py
@@ -0,0 +1,26 @@
+def main():
+    a = name(input("Name?: ")).lower()
+    b = number(int(input("b: ")))
+
+    print(f"The name is: {a}")
+    print(f"The number is: {b}")
+
+def name(person):
+    if person != "anik":
+      return ("Hello,"+ " "+ person)
+    else:
+      return ("Hello, anik" + "Where is the person? ")
+
+
+def number(x):
+    if x == 0:
+       x += 2
+       return x
+    elif x == 100:
+      x = x-1
+      return x
+    else:
+      return x
+    
+
+main()
