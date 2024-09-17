4dd990285b970987d3d6f12a5586a3794d8d4d82
Anik Rahman Chowdhury <111109249+anikrahman007@users.noreply.github.com>
2024-09-17 13:55:53 +0600

    variable_01.py

--- PYTHON/variable_01.py
+++ PYTHON/variable_01.py
@@ -1,26 +1,28 @@
+ban_name = input("Banned Name: ").lower()
 def main():
+    global ban_name
     a = name(input("Name?: ")).lower()
-    b = number(int(input("b: ")))
+    b = age(int(input("b: ")))
 
     print(f"The name is: {a}")
-    print(f"The number is: {b}")
+    print(f"The age is: {b}")
 
 def name(person):
-    if person != "anik":
+    if person != ban_name:
       return ("Hello,"+ " "+ person)
     else:
       return ("Hello, anik" + " " + "Where is the person? ")
 
 
-def number(x):
+def age(x):
     if x == 0:
-       x += 2
+       x += 18
        return x
     elif x == 100:
-      x = x-1
+      x = x-20
       return x
     else:
       return x
-    
+
 
 main()
